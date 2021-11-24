1a800290b73b2fcd8260f7a54f9e9688f0b8816f
airzt11 <airzonatea@gmail.com>
2021-11-24 03:48:24 -0500

    Fix syntax in authentication_token_service.rb

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,6 +1,6 @@
 # moving JWT encode and decode methods to application controller
 
-# class AuthenticationTokenService
+class AuthenticationTokenService
 #   HMAC_SECRET = 'my$ecretK3y'
 #   ALGORITHM_TYPE = 'HS256'
 
@@ -14,4 +14,4 @@
 #     decoded_token = JWT.decode token, HMAC_SECRET, true, { algorithm: ALGORITHM_TYPE }
 #     decoded_token[0]['user_id']
 #   end
-# end
+end
